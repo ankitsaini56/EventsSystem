3a01e3fc6c4765b32c9f3fb8ebe47e4f76745901
ankitsaini56 <ankit.saini@56secure.com>
2023-05-23 22:27:40 +0530

    Initial Commit

---
+++ EventsSystem/EventsSystem.h
@@ -0,0 +1,18 @@
+//
+//  EventsSystem.h
+//  EventsSystem
+//
+//  Created by Ankit Saini on 23/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EventsSystem.
+FOUNDATION_EXPORT double EventsSystemVersionNumber;
+
+//! Project version string for EventsSystem.
+FOUNDATION_EXPORT const unsigned char EventsSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EventsSystem/PublicHeader.h>
+
+
